a87d2cc7cf9706663e316bcda8e1faf6757ca4ff
Moyeong Lee <2000prcs@gmail.com>
2020-11-24 14:20:03 -0800

    Initial structure

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,6 +6,9 @@ const preferDefault = m => (m && m.default) || m
 
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/44982/Documents/Personal/homepage/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/44982/Documents/Personal/homepage/src/pages/404.js")))
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/44982/Documents/Personal/homepage/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/44982/Documents/Personal/homepage/src/pages/index.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/44982/Documents/Personal/homepage/src/pages/page-2.js"))),
+  "component---src-pages-using-typescript-tsx": hot(preferDefault(require("/Users/44982/Documents/Personal/homepage/src/pages/using-typescript.tsx")))
 }
 
