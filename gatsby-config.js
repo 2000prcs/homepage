f18b27c1a0cc943a3a8fe4759288afce162a6485
Moyeong Lee <2000prcs@gmail.com>
2020-11-24 14:03:30 -0800

    Initial structure

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Hi I'm Mo`,
+    description: `I'm a Software Engineer`,
+    author: `@mo`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
