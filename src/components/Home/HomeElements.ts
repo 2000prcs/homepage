3e1dac1ab71f3291e8be73c8efe905bf42dbe884
Moyeong Lee <2000prcs@gmail.com>
2021-02-16 15:54:01 -0800

    move image components to home elements and define types for ref and style

--- src/components/Home/HomeElements.ts
+++ src/components/Home/HomeElements.ts
@@ -1,4 +1,26 @@
 import styled from 'styled-components';
+import FishClusterImg from '../../images/fish_cluster.svg';
+import TwoFishesImg from '../../images/two_fishes.svg';
+import TurtleImg from '../../images/turtle.svg';
+import FuguImg from '../../images/fugu.svg';
+import DolphinImg from '../../images/dolphin.svg';
+import FishOneImg from '../../images/fish1.svg';
+import FishTwoImg from '../../images/fish2.svg';
+import WaleImg from '../../images/wale.svg';
+import SeaHorseImg from '../../images/sea_horse.svg';
+import BabySharkImg from '../../images/baby_shark.svg';
+import CrabImg from '../../images/crab.svg';
+import MermaidTwoImg from '../../images/mermaid2.svg';
+import OctopusImg from '../../images/octopus.svg';
+import StarFishImg from '../../images/starfish.svg';
+import MermaidThreeImg from '../../images/mermaid3.svg';
+import SquidImg from '../../images/squid.svg';
+import AnglerFishImg from '../../images/angler_fish.svg';
+import JellyfishImg from '../../images/jellyfish.svg';
+import MermaidFourImg from '../../images/mermaid4.svg';
+import EllImg from '../../images/ell.svg';
+import LobsterImg from '../../images/lobster.svg';
+import MermaidOneImg from '../../images/mermaid1.svg';
 
 export const HeroContainer = styled.div`
   background: #ffffff;
@@ -9,4 +31,141 @@ export const HeroContainer = styled.div`
   height: 800px;
   position: relative;
   z-index: 1;
+`
+
+const styledImg = `
+  display: block;
+  width: 50%;
+`
+
+export const StyledFishClusterImg = styled(FishClusterImg)`
+  ${styledImg}
+  margin-left: 55%;
+  margin-top: -12%;
+`
+
+export const StyledTwoFishesImg = styled(TwoFishesImg)`
+  ${styledImg}
+  margin-left: 60%;
+  margin-top: -30%;
+`
+
+export const StyledTurtleImg = styled(TurtleImg)`
+  ${styledImg}
+  margin-left: 0%;
+  margin-top: 10%;
+`
+
+export const StyledFuguImg = styled(FuguImg)`
+  ${styledImg}
+  margin-left: 5%;
+  margin-top: -30%;
+`
+
+export const StyledDolphinImg = styled(DolphinImg)`
+  ${styledImg}
+  margin-left: 0%;
+  margin-top: 70%;
+`
+
+export const StyledFishOneImg = styled(FishOneImg)`
+  ${styledImg}
+  margin-left: -5%;
+  margin-top: 15%;
+`
+
+export const StyledFishTwoImg = styled(FishTwoImg)`
+  ${styledImg}
+  margin-left: 50%;
+  margin-top: -23%;
+`
+
+export const StyledWaleImg = styled(WaleImg)`
+  ${styledImg}
+  margin-left: 5%;
+  margin-top: 10%;
+`
+
+export const StyledSeaHorseImg = styled(SeaHorseImg)`
+  ${styledImg}
+  margin-left: 50%;
+  margin-top: 35%;
+`
+
+export const StyledBabySharkImg = styled(BabySharkImg)`
+  ${styledImg}
+  margin-left: 30%;
+  margin-top: -30%;
+`
+
+export const StyledCrabImg = styled(CrabImg)`
+  ${styledImg}
+  margin-left: -15%;
+  margin-top: -21%;
+`
+
+export const StyledMermaidTwoImg = styled(MermaidTwoImg)`
+  display: block;
+  margin-left: 5%;
+  margin-top: -25%;
+  width: 15%;
+`
+
+export const StyledSquidImg = styled(SquidImg)`
+  ${styledImg}
+  margin-left: 40%;
+  margin-top: 2%;
+`
+
+export const StyledAnglerFishImg = styled(AnglerFishImg)`
+  ${styledImg}
+  margin-left: 0%;
+  margin-top: 5%;
+`
+
+export const StyledOctopusImg = styled(OctopusImg)`
+  ${styledImg}
+  margin-left: 30%;
+  margin-top: 15%;
+`
+
+export const StyledStarFishImg = styled(StarFishImg)`
+  ${styledImg}
+  margin-left: 40%;
+  margin-top: 10%;
+`
+
+export const StyledMermaidThreeImg = styled(MermaidThreeImg)`
+  display: block;
+  margin-left: 70%;
+  width: 15%;
+`
+
+export const StyledJellyfishImg = styled(JellyfishImg)`
+  ${styledImg}
+  margin-left: 70%;
+`
+
+export const StyledMermaidFourImg = styled(MermaidFourImg)`
+  display: block;
+  margin-left: 60%;
+  width: 15%;
+`
+
+export const StyledEllImg = styled(EllImg)`
+  ${styledImg}
+  margin-left: 35%;
+  margin-top: 8%;
+`
+
+export const StyledLobsterImg = styled(LobsterImg)`
+  ${styledImg}
+  margin-left: 10%;
+  margin-top: 0%;
+`
+
+export const StyledMermaidOneImg = styled(MermaidOneImg)`
+  display: block;
+  margin-left: 15%;
+  width: 15%;
 `
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -6,34 +6,35 @@ import HeroSection from '../HeroSection';
 import ProfileSection from '../AboutSection';
 import PortfolioSection from '../PortfolioSection';
 import ContactSection from '../ContactSection';
-
-import FishOneImg from '../../images/fish1.svg';
-import FishTwoImg from '../../images/fish2.svg';
-import TwoFishesImg from '../../images/two_fishes.svg';
-import FishClusterImg from '../../images/fish_cluster.svg';
-import JellyfishImg from '../../images/jellyfish.svg';
-import CrabImg from '../../images/crab.svg';
-import DolphinImg from '../../images/dolphin.svg';
-import OctopusImg from '../../images/octopus.svg';
-import SeaHorseImg from '../../images/sea_horse.svg';
-import StarFishImg from '../../images/starfish.svg';
-import SquidImg from '../../images/squid.svg';
-import TurtleImg from '../../images/turtle.svg';
-import WaleImg from '../../images/wale.svg';
-import EllImg from '../../images/ell.svg';
-import LobsterImg from '../../images/lobster.svg';
-import AnglerFishImg from '../../images/angler_fish.svg';
-import FuguImg from '../../images/fugu.svg';
-import BabySharkImg from '../../images/baby_shark.svg';
-import MermaidOneImg from '../../images/mermaid1.svg';
-import MermaidTwoImg from '../../images/mermaid2.svg';
-import MermaidThreeImg from '../../images/mermaid3.svg';
-import MermaidFourImg from '../../images/mermaid4.svg';
+import {
+  StyledFishClusterImg,
+  StyledTwoFishesImg,
+  StyledTurtleImg,
+  StyledFuguImg,
+  StyledDolphinImg,
+  StyledFishOneImg,
+  StyledFishTwoImg,
+  StyledWaleImg,
+  StyledSeaHorseImg,
+  StyledBabySharkImg,
+  StyledCrabImg,
+  StyledMermaidTwoImg,
+  StyledSquidImg,
+  StyledAnglerFishImg,
+  StyledOctopusImg,
+  StyledStarFishImg,
+  StyledMermaidThreeImg,
+  StyledJellyfishImg,
+  StyledMermaidFourImg,
+  StyledEllImg,
+  StyledLobsterImg,
+  StyledMermaidOneImg
+} from './HomeElements';
 
 import BackgroundImg from '../../images/underwater.png';
 
 const Home:React.FC = () => {
-  const parallaxRef = useRef();
+  const parallaxRef = useRef<Parallax>(null);
   const [isOpen, setIsOpen] = useState(false);
   const [currentPage, setCurrentPage] = useState(0);
 
@@ -68,59 +69,49 @@ const Home:React.FC = () => {
       <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#0367a6', opacity: 0.6 }} />
       <ParallaxLayer offset={3} speed={1} style={{ backgroundColor: '#012340', opacity: 0.6 }} />
       <ParallaxLayer offset={0} speed={-0.3}>
-        <FishClusterImg style={{ display: 'block', width: '50%', marginLeft: '55%', marginTop: '-12%'}} />
-        <TwoFishesImg style={{ display: 'block', width: '50%', marginLeft: '60%', marginTop: '-30%'}} />
+        <StyledFishClusterImg />
+        <StyledTwoFishesImg />
       </ParallaxLayer>
       <ParallaxLayer offset={0.3} speed={0.3}>
-        <TurtleImg style={{ display: 'block', width: '50%', marginLeft: '0%', marginTop: '10%' }} />
-        <FuguImg style={{ display: 'block', width: '50%', marginLeft: '5%', marginTop: '-30%' }} />
-        <DolphinImg style={{ display: 'block', width: '50%', marginLeft: '0%', marginTop: '70%' }} />
+        <StyledTurtleImg />
+        <StyledFuguImg />
+        <StyledDolphinImg />
       </ParallaxLayer>
-
       <ParallaxLayer offset={0.3} speed={0.8}>
-        <FishOneImg style={{ display: 'block', width: '50%', marginLeft: '-5%', marginTop: '15%' }} />
-        <FishTwoImg style={{ display: 'block', width: '50%', marginLeft: '50%', marginTop: '-23%' }} />
+      <StyledFishOneImg />
+      <StyledFishTwoImg />
       </ParallaxLayer>
-
       <ParallaxLayer offset={0.5} speed={0.5}>
-        <WaleImg style={{ display: 'block', width: '50%', marginLeft: '5%', marginTop: '10%' }} />
+        <StyledWaleImg />
       </ParallaxLayer>
-
       <ParallaxLayer offset={1} speed={0.2}>
-        <SeaHorseImg style={{ display: 'block', width: '50%', marginLeft: '50%', marginTop: '35%' }} />
-        <BabySharkImg style={{ display: 'block', width: '50%', marginLeft: '30%', marginTop: '-30%' }} />
+        <StyledSeaHorseImg />
+        <StyledBabySharkImg />
       </ParallaxLayer>
-
       <ParallaxLayer offset={1} speed={0.8}>
-        <CrabImg style={{ display: 'block', width: '50%', marginLeft: '-15%', marginTop: '-21%' }} />
-        <MermaidTwoImg style={{ display: 'block', width: '15%', marginLeft: '5%', marginTop: '-25%' }} />
+        <StyledCrabImg />
+        <StyledMermaidTwoImg />
       </ParallaxLayer>
-
-      <ParallaxLayer offset={1.75} speed={0.5}>
-        <OctopusImg style={{ display: 'block', width: '50%', marginLeft: '30%', marginTop: '15%' }} />
-        <StarFishImg style={{ display: 'block', width: '50%', marginLeft: '40%', marginTop: '10%' }} />
-        <MermaidThreeImg style={{ display: 'block', width: '15%', marginLeft: '70%' }} />
-      </ParallaxLayer>
-
       <ParallaxLayer offset={1.6} speed={-0.1}>
-        <SquidImg style={{ display: 'block', width: '50%', marginLeft: '40%', marginTop: '2%' }} />
-        <AnglerFishImg style={{ display: 'block', width: '50%', marginLeft: '0%', marginTop: '5%' }} />
+        <StyledSquidImg />
+        <StyledAnglerFishImg />
       </ParallaxLayer>
-
-      <ParallaxLayer offset={2.6} speed={0.4}>
-        <MermaidFourImg style={{ display: 'block', width: '15%', marginLeft: '60%' }} />
-        <EllImg style={{ display: 'block', width: '50%', marginLeft: '35%', marginTop: '8%' }} />
+      <ParallaxLayer offset={1.75} speed={0.5}>
+        <StyledOctopusImg />
+        <StyledStarFishImg />
+        <StyledMermaidThreeImg />
       </ParallaxLayer>
-
-      <ParallaxLayer offset={2.6} speed={0.3}>
-        <LobsterImg style={{ display: 'block', width: '50%', marginLeft: '10%', marginTop: '0%' }} />
-        <MermaidOneImg style={{ display: 'block', width: '15%', marginLeft: '15%' }} />
-      </ParallaxLayer>
-
       <ParallaxLayer offset={2.5} speed={-0.4}>
-        <JellyfishImg style={{ display: 'block', width: '50%', marginLeft: '70%' }} />
+        <StyledJellyfishImg />
       </ParallaxLayer>
-
+      <ParallaxLayer offset={2.6} speed={0.4}>
+        <StyledMermaidFourImg />
+        <StyledEllImg />
+      </ParallaxLayer>
+      <ParallaxLayer offset={2.6} speed={0.3}>
+        <StyledLobsterImg />
+        <StyledMermaidOneImg />
+      </ParallaxLayer>
       <ParallaxLayer
         offset={0}
         speed={0.1}
