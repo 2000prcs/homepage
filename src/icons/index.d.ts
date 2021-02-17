1431b11ecdc10c14c9844db07f6a17e230d3d886
Moyeong Lee <2000prcs@gmail.com>
2021-02-17 15:47:02 -0800

    add type declaration in icons directory

--- src/icons/index.d.ts
+++ src/icons/index.d.ts
@@ -1,8 +1,3 @@
-declare module '\*.png' {
-  const content: any;
-  export default content;
-}
-
 declare module '\*.svg' {
   import { ReactElement, SVGProps } from 'react';
   const content: (props: SVGProps<SVGElement>) => ReactElement;
